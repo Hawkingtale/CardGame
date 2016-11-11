e241913818057f872e342e507231e1e253ab743a
Hawkingtale <mhac5527@740s-pl-173.local>
2016-11-11 13:54:30 -0700

    Card game now has the matching game

---
+++ CardGameUpdate/StupidGame.swift
@@ -0,0 +1,27 @@
+//
+//  StupidGame.swift
+//  CardGameUpdate
+//
+//  Created by Hachtel, Matthew on 11/11/16.
+//  Copyright © 2016 CTECH. All rights reserved.
+//
+
+import Foundation
+
+class StupidGame
+    
+
+{
+    //internal var nameOfVariable : Type
+    internal var gameDeck : PlayingCardDeck
+    internal var Rank : int
+    init()
+    {
+        gameDeck = PlayingCardDeck()
+    }
+    
+    func startGame() -> Void
+    {
+        
+    }
+}
